8052b79e8054aab13ce4939c17e57da15a56121e
grecyaDangio <grecya.dangio01@universitadipavia.it>
2022-05-07 00:34:22 +0200

    folder

--- modello_cubico.m
+++
@@ -1,9 +0,0 @@
-
-close all;
-clc; 
-clear;
-turbina= load ('clean_dataset.mat');
-train_data = readtable('train.csv');
-test_data = readtable('test.csv');
-
-
--- validazione.m
+++
@@ -1,6 +0,0 @@
-%dati di validazione 
-close all;
-clc; 
-clear;
-turbina= load ('clean_dataset.mat');
-
