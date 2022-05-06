3b5ed6998115de204d5d33124d705b0d24e1c5f9
grecyaDangio <grecya.dangio01@universitadipavia.it>
2022-05-06 23:19:28 +0200

    grafico

--- prova2.m
+++ prova2.m
@@ -1,15 +1,17 @@
-close all; 
+
+close all;
 clc; 
 clear;
-turbine =load('clean_dataset.mat');
+turbina= load ('clean_dataset.mat');
 dati_grezzi = readtable('train.csv');
 
+
 figure;
-scatter([dati_grezzi.ws; dati_grezzi.ws], [dati_grezzi.turbine_wp1; dati_grezzi.turbine_wp1], '.', 'MarkerEdgeColor', '#D95319');
+scatter(dati_grezzi.ws,dati_grezzi.wp1, '.', 'MarkerEdgeColor', '#D95319');
 hold on;
-scatter(turbine.ws, turbine.wp1, '.', 'MarkerEdgeColor', '#0072BD');
+scatter(turbina.turbine.ws, turbina.turbine.wp1, '.', 'MarkerEdgeColor', '#0072BD');
 grid on;
-title('Clean data from both turbine', 'Interpreter', 'latex');
-xlabel('Wind Speed [m/s]', "Interpreter", 'latex');
-ylabel('Generated Power [kW]', "Interpreter", 'latex');
-legend("Detected Outliers", "Clean Data", "Interpreter", 'latex');
+title('Clean data from the turbine');
+xlabel('Wind Speed [m/s]');
+ylabel('Generated Power [kW]');
+legend("Detected Outliers", "Clean Data");
